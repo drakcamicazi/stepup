8982542f056f5eb179b9b67839d04b1f69ee0c58
Unknown <dfonseca404@gmail.com>
2018-11-22 21:18:53 -0400

    removing all venv files

--- venv/App/form.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-#from .models import Pessoa
-
-# Exemplo de gerador de formulário
-#class PessoaForm(ModelForm):
-#    class Meta:
-#        model = Pessoa
-#        fields = ['nome', 'idade', 'cpf', 'genero', 'email']
